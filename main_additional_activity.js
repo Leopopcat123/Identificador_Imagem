b4f9819f97e5c417b3e8bf792f1c927ecf21e660
Leopopcat123 <113531806+Leopopcat123@users.noreply.github.com>
2023-06-06 17:29:23 -0300

    Update main_additional_activity.js

--- main_additional_activity.js
+++ main_additional_activity.js
@@ -20,7 +20,7 @@ function take_snapshot()
 
   console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/VK6X7UYHV/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-p7tQwP46/model.json',modelLoaded);
 
   function modelLoaded() {
     console.log('Modelo Carregado!');
